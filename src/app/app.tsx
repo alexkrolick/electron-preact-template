3f6aef3b71f473ac4da5f3c3b8a7429c5ab06c7b
Alex Krolick <alexander.krolick@gmail.com>
2020-04-18 20:02:26 -0700

    init

---
+++ src/app/app.tsx
@@ -0,0 +1,7 @@
+import { h } from "preact";
+
+function App() {
+  return <h2 style={{ fontFamily: "Source Sans Pro" }}>preact app</h2>;
+}
+
+export default App;
---
+++ webpack.renderer.config.js
@@ -0,0 +1,26 @@
+const rules = require("./webpack.rules");
+const plugins = require("./webpack.plugins");
+
+rules.push({
+  test: /\.css$/,
+  use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+});
+
+rules.push({
+  test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+  use: [
+    {
+      loader: "file-loader",
+    },
+  ],
+});
+
+module.exports = {
+  module: {
+    rules,
+  },
+  plugins: plugins,
+  resolve: {
+    extensions: [".js", ".ts", ".jsx", ".tsx", ".css"],
+  },
+};
